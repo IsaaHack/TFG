9e68dabfb8f47565939603cf047f1f7203a68ecc
IsaaHack <isaacbrao@correo.ugr.es>
2025-04-02 13:57:11 +0000

    Añadida salida rtx 3060

--- algoritms/algoritm.py
+++ algoritms/algoritm.py
@@ -2,5 +2,9 @@ from abc import ABC, abstractmethod
 
 class Algoritm(ABC):
     @abstractmethod
+    def check_required_methods(self):
+        pass
+
+    @abstractmethod
     def fit(self):
         pass
